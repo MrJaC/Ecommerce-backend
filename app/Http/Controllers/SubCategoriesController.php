ab3db21cd251602bec3d9158b4ed3ca816b74e7a
Jodi <jodicoetzee@live.com>
2020-02-12 14:15:04 +0200

    updates model and controller

--- app/Http/Controllers/SubCategoriesController.php
+++ app/Http/Controllers/SubCategoriesController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class SubCategoriesController extends Controller
 {
-    //
+        /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Show the subcategories.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('subcategories/subcategories');
+    }
 }
