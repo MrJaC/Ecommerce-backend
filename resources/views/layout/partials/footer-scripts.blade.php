0f0f704e128e1ae1c958c81b3393a230272d95c7
Jodi <jodicoetzee@live.com>
2020-02-12 14:28:11 +0200

    datatable scripts add

--- resources/views/layout/partials/footer-scripts.blade.php
+++ resources/views/layout/partials/footer-scripts.blade.php
@@ -7,7 +7,9 @@
 <script src="{!! ('plugins/bootstrap/js/bootstrap.bundle.min.js') !!}"></script>
 <!-- AdminLTE -->
 <script src="{!! ('js/adminlte.js') !!}"></script>
-
+<!-- DataTables -->
+<script src="{!! ('plugins/datatables/jquery.dataTables.js') !!}"></script>
+<script src="{!! ('plugins/datatables-bs4/js/dataTables.bootstrap4.js') !!}"></script>
 <!-- OPTIONAL SCRIPTS -->
 <script src="{!! ('plugins/chart.js/Chart.min.js') !!}"></script>
 <script src="{!! ('js/demo.js') !!}"></script>
