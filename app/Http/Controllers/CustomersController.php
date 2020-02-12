4a0414d2386d6e9e964e76004b18824351ef4598
Jodi <jodicoetzee@live.com>
2020-02-12 16:43:20 +0200

    updated customers controller

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -6,5 +6,26 @@ use Illuminate\Http\Request;
 
 class CustomersController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Show the categories.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('customers/customers');
+    }
+    public function create()
+    {
+        return view('customers/create-customers');
+    }
 }
