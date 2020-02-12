7b58683cd5a66209152cb8441743724151f58890
Jodi <jodicoetzee@live.com>
2020-02-12 14:27:07 +0200

    datatable css add

--- resources/views/layout/partials/head.blade.php
+++ resources/views/layout/partials/head.blade.php
@@ -8,6 +8,8 @@
 <link rel="stylesheet" href="{!! ('plugins/fontawesome-free/css/all.min.css') !!}">
 <!-- IonIcons -->
 <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="{!! ('plugins/datatables-bs4/css/dataTables.bootstrap4.css') !!}">
 <!-- Theme style -->
 <link rel="stylesheet" href="{!! ('css/adminlte.min.css') !!}">
 <!-- Google Font: Source Sans Pro -->
