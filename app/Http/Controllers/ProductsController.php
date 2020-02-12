29d648d5ad00eabe8b705b05cdb55ca0d3b528ab
Jodi <jodicoetzee@live.com>
2020-02-12 14:24:20 +0200

    updates to template

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    //
+        /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application products.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('products/products');
+    }
 }
