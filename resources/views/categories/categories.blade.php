170839b744b90a730eb8cf958b5f507ad831cd5e
Jodi <jodicoetzee@live.com>
2020-02-12 18:38:17 +0200

    template updates

--- resources/views/categories/categories.blade.php
+++ resources/views/categories/categories.blade.php
@@ -55,11 +55,11 @@
                                 <button type="button" class="btn btn-default dropdown-toggle dropdown-icon" data-toggle="dropdown">
                                   <span class="sr-only">Toggle Dropdown</span>
                                   <div class="dropdown-menu" role="menu">
-                                    <a class="dropdown-item" href="#">Action</a>
+                                  <a class="dropdown-item" href="/edit">Edit</a>
                                     <a class="dropdown-item" href="#">Another action</a>
                                     <a class="dropdown-item" href="#">Something else here</a>
                                     <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="#">Separated link</a>
+                                    <a class="dropdown-item" href="/delete">Delete</a>
                                   </div>
                                 </button>
                               </div></td>
--- resources/views/customers/customers.blade.php
+++ resources/views/customers/customers.blade.php
@@ -57,11 +57,11 @@
                                 <button type="button" class="btn btn-default dropdown-toggle dropdown-icon" data-toggle="dropdown">
                                   <span class="sr-only">Toggle Dropdown</span>
                                   <div class="dropdown-menu" role="menu">
-                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Edit</a>
                                     <a class="dropdown-item" href="#">Another action</a>
                                     <a class="dropdown-item" href="#">Something else here</a>
                                     <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="#">Separated link</a>
+                                    <a class="dropdown-item" href="#">Delete</a>
                                   </div>
                                 </button>
                               </div></td>
