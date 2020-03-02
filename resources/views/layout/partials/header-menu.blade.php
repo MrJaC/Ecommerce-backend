8803768ff562d42a84dadc1b94da6db39a4a43c2
Jodi <jodicoetzee@live.com>
2020-03-02 16:47:34 +0200

    updates

--- resources/views/layout/partials/header-menu.blade.php
+++ resources/views/layout/partials/header-menu.blade.php
@@ -8,9 +8,7 @@
       <li class="nav-item d-none d-sm-inline-block">
         <a href="/" class="nav-link">Home</a>
       </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
-      </li>
+
       @if(Auth::user()->role == 4)
       <li class="nav-item d-none d-sm-inline-block">
         <a href="/vendor-profile" class="nav-link">Vendor Profile</a>
--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -30,7 +30,7 @@
               <div class="card-body box-profile">
                 <div class="text-center">
                   <img class="profile-user-img img-fluid img-circle"
-                       src="../../dist/img/user4-128x128.jpg"
+                  src="{{ url('storage/user/user-images/'.$user->user_image) }}"
                        alt="User profile picture">
                 </div>
 
