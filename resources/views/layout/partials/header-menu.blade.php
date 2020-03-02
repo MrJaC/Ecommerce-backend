2ef5c12c674178ce19721f0498b95ff79048af21
Jodi <jodicoetzee@live.com>
2020-03-02 10:23:19 +0200

    vendor profile menu

--- resources/views/layout/partials/header-menu.blade.php
+++ resources/views/layout/partials/header-menu.blade.php
@@ -11,6 +11,11 @@
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
       </li>
+      @if(Auth::user()->role == 4)
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="#" class="nav-link">Vendor Profile</a>
+      </li>
+      @endif
     </ul>
 
     <!-- SEARCH FORM -->
