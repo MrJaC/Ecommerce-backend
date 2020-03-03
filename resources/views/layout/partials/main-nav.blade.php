e78b643335145475a68689508f7d66c9daed035e
Jodi <jodicoetzee@live.com>
2020-03-03 16:02:58 +0200

    updated menu

--- resources/views/layout/partials/main-nav.blade.php
+++ resources/views/layout/partials/main-nav.blade.php
@@ -186,6 +186,31 @@
                       <p>Create</p>
                     </a>
                   </li>
+                </ul>
+                </li>
+                <li class="nav-item has-treeview menu-closed">
+                    <a href="#" class="nav-link active">
+                      <i class="nav-icon fas fa-cogs"></i>
+                      <p>
+                        Staff
+                        <i class="right fas fa-angle-left"></i>
+                      </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                      <li class="nav-item">
+                        <a href="/customers" class="nav-link">
+                          <i class="far fa-circle nav-icon"></i>
+                          <p>View </p>
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="/create-customers" class="nav-link">
+                          <i class="far fa-circle nav-icon"></i>
+                          <p>Create</p>
+                        </a>
+                      </li>
+                    </ul>
+                    </li>
 
         </ul>
       </nav>
