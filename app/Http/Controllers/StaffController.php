8d0a36be3885b7315ce5727a24675714a6c1f49f
Jodi <jodicoetzee@live.com>
2020-03-05 13:00:08 +0200

    updates to staff

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Staff;
+
 
 class StaffController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index(){
+        $data = app(Staff::class)->getStaff();
+
+        return view('staff/staff', ['staff'=> $data]);
+    }
 }
---
+++ resources/views/staff/staff.blade.php
@@ -0,0 +1,107 @@
+@extends('layout.main')
+@section('content')
+      <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Staff</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="/">Home</a></li>
+              <li class="breadcrumb-item">Staff</li>
+              <li class="breadcrumb-item active">View</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        <section class="content">
+            <div class="row">
+                <div class="col-12">
+                  <div class="card">
+                    <div class="card-header">
+                      <h3 class="card-title">View all Staff</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                      <table id="example2" class="table table-bordered table-hover">
+                        <thead>
+                        <tr>
+                          <th>ID</th>
+                          <th>Staff Name</th>
+                          <th>Staff Email</th>
+                          <th>Created at:</th>
+                          <th>Role</th>
+                          <th>Actions</th>
+
+                        </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($customers as $cus)
+                            <tr>
+                            <td>{{$cus->id}}</td>
+                            <td>{{$cus->name}}</td>
+                            <td>{{$cus->email}}</td>
+                            <td>{{$cus->created_at}}</td>
+                            <td>
+                                @if ($cus->role == 1)
+                                 Admin
+                            @elseif ($cus->role == 2)
+                                Employee
+                                @elseif ($cus->role == 3)
+                                Staff
+                                @elseif ($cus->role == 4)
+                                Vendor
+                            @endif
+
+                            </td>
+                            <td>
+                                <div class="btn-group">
+                                <button type="button" class="btn btn-default">Action</button>
+                                <button type="button" class="btn btn-default dropdown-toggle dropdown-icon" data-toggle="dropdown">
+                                  <span class="sr-only">Toggle Dropdown</span>
+                                  <div class="dropdown-menu" role="menu">
+                                    <a class="dropdown-item" href="#">Edit</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Delete</a>
+                                  </div>
+                                </button>
+                              </div></td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                        <tfoot>
+                        <tr>
+                            <th>ID</th>
+                            <th>Staff Name</th>
+                            <th>Staff Email</th>
+                            <th>Created at:</th>
+                            <th>Role</th>
+                            <th>Actions</th>
+                        </tr>
+                        </tfoot>
+                      </table>
+                    </div>
+                    <!-- /.card-body -->
+                  </div>
+                  <!-- /.card -->
+        </section>
+        <!-- /.row -->
+      </div>
+      <!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+@endsection
