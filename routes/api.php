6950e05b9b682c161612280fd853cdf1c7c9ed73
Jodi <jodicoetzee@live.com>
2020-03-25 13:19:33 +0200

    auth stuff

--- routes/api.php
+++ routes/api.php
@@ -13,17 +13,20 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//AUTH GROUP
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'Auth\AuthController@login')->name('login');
+    Route::post('register', 'Auth\AuthController@register');
+    Route::group([
+      'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'Auth\AuthController@logout');
+        Route::get('user', 'Auth\AuthController@user');
+    });
 });
 
-//Auth
-
-Route::post('login', 'UserController@login');
-Route::post('register', 'UserController@register');
-Route::group(['middleware' => 'auth:api'], function () {
-    Route::post('details', 'UserController@details');
-});
 //API GROUP
 Route::group(['prefix' => 'v1', 'name' => 'v1'], function(){
     //Categories
