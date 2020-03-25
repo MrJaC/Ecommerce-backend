1ded6502aa075df567c6badc3298a23457820a72
Jodi <jodicoetzee@live.com>
2020-03-25 17:43:00 +0200

    update authcontroller api

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,12 +7,12 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Hash;
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        error_log(print_r($request,true));
+
         $request->validate([
             'email' => 'required|string|email',
             'password' => 'required|string',
@@ -20,7 +20,7 @@ class AuthController extends Controller
         ]);
 
         $credentials = request(['email', 'password']);
-        error_log(print_r($credentials,true));
+
         if (!Auth::attempt($credentials))
             return response()->json([
                 'message' => 'Unauthorized'
@@ -48,10 +48,10 @@ class AuthController extends Controller
             'password' => 'required|string'
         ]);
         $user = new User;
-        $user->first_name = $request->fName;
-        $user->last_name = $request->lName;
+        $user->name = $request->fName." ".$request->lName;
         $user->email = $request->email;
-        $user->password = bcrypt($request->password);
+        $user->password = Hash::make($request->password);
+        $user->role = 3;
         $user->save();
         return response()->json([
             'message' => 'Successfully created user!'
